bc36e62fd08df0f5be039bcb8b79011e41093845
IramMcFly <contactomcfly@gmail.com>
2025-05-24 01:45:36 -0600

    page adjust for render

--- app/riego/page.js
+++ app/riego/page.js
@@ -1,7 +1,13 @@
 "use client";
 
-import MapaRiegos from "@/components/MapaRiegos";
+import dynamic from "next/dynamic";
 import Header from "@/components/Header";
+
+// ⛔️ NO: import MapaRiegos from "@/components/MapaRiegos"
+// ✅ SÍ: importación dinámica sin SSR
+const MapaRiegos = dynamic(() => import("@/components/MapaRiegos"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
