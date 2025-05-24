b15b3ea1659f157dc15c29ece27e5ed403290471
EDMG <edwind.m115@gmail.com>
2025-05-24 01:11:18 -0600

    Modificacion Visual en SensorStatus

--- app/sensores/page.js
+++ app/sensores/page.js
@@ -2,6 +2,10 @@ import SensorStatus from "@/components/SensorStatus";
 
 export default function Home() {
   return (
-    <SensorStatus />
-    );
+    <div className="min-h-screen bg-white p-4 flex items-center justify-center">
+      <div className="w-full sm:w-2/3 md:w-2/3 lg:w-2/3 xl:w-2/3 flex justify-center">
+        <SensorStatus />
+      </div>
+    </div>
+  );
 }
